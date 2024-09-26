6173b8bafa48c132281b170eb61bc5bf2b2d69c2
Ashutosh-pixel <ashushar222@gmail.com>
2024-09-26 19:35:59 +0530

    folders rename feature added

--- src/component/FoldersContainer.jsx
+++ src/component/FoldersContainer.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useStore from '../store/Store';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { event } from '@tauri-apps/api';
 
 export default function FoldersContainer() {
 
-  const folderArray = useStore((state) => state.folderArray);
-  const setSelectedCategory = useStore((state) => state.setSelectedCategory);
+  let folderArray = useStore((state) => state.folderArray);
 
 
-  const clickHandler = (newcategory) => {
-    setSelectedCategory(newcategory);
+  const [isEditable, setIsEditable] = useState(Array(folderArray.length).fill(false));
+  const [duplicateArray, setDuplicateArray] = useState(['All']);
+
+
+  const clickHandler = (e, index) => {
+    const updatedArray = [...folderArray];
+    const newvalue = e.target.value;
+    updatedArray[index] = e.target.value;
+    setDuplicateArray(updatedArray);
+    folderArray[index] = e.target.value;
+    console.log("folderArray ", folderArray);
   }
+
+  const toggleEditable = (index, item) => {
+    const updatedEditableState = [...isEditable];
+    updatedEditableState[index] = !updatedEditableState[index];  // Toggle the specific item's editability
+    setIsEditable(updatedEditableState);
+    folderArray[index] = item.trim();
+  };
 
   return (
     <div className='folderscontainer mt-4 w-full flex flex-col gap-2 select-none'>
       {folderArray.map((item, index) => {
-        return <Link to={'/'}><div key={index} onClick={() => clickHandler(item)} className='parent w-full bg-white rounded-md h-10 flex justify-center items-center shadow-md cursor-pointer hover:bg-slate-100'>
-          <div className='w-11/12 flex justify-between items-center'>
-            <span className='text-lg'>
-              {item}
-            </span>
-            <span className='text-sm'>
-              0
-            </span>
-          </div>
+        return <div key={index} className='parent w-full bg-white rounded-md h-10 flex justify-center items-center shadow-md cursor-pointer'>
+          <span className='w-28 flex justify-between items-center'>
+            <input value={item} onChange={(e) => clickHandler(e, index)} readOnly={!isEditable[index]} className='w-full' type="text" name="" id="" />
+          </span>
+          <span className='text-sm' onClick={() => toggleEditable(index, item)}>
+            {isEditable[index] ? 'Save' : 'Rename'}
+          </span>
         </div>
-        </Link>
       })}
-    </div>
+    </div >
   )
 }
